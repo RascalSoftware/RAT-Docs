b3501a8281b56b33ff2b29bcb4f200799aa17214
Stephen Nneji <steve.nneji@gmail.com>
2024-11-15 13:03:05 +0000

    Adds deploy script

--- deploy.py
+++ deploy.py
@@ -7,8 +7,7 @@ from version import get_doc_version
 
 DOCS_PATH = os.path.abspath(os.path.dirname(__file__))
 
-url = os.environ.get('RAT_URL', '') 
-print('RAT_URL: ', url)
+url = os.environ.get('RAT_URL', '')
 doc_version = get_doc_version()
 
 BUILD_PATH = os.path.join(DOCS_PATH, 'build', 'html')
